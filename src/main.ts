593de7cdd921f000b2b72891a7b5e42a346c41aa
Andy Krings-Stern <ankri@users.noreply.github.com>
2019-09-05 16:38:37 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+import { GitHubContext } from '@tangro/tangro-github-toolkit';
+
+async function run() {
+  try {
+    if (
+      !process.env.GITHUB_CONTEXT ||
+      process.env.GITHUB_CONTEXT.length === 0
+    ) {
+      throw new Error(
+        'You have to set the GITHUB_CONTEXT in your secrets configuration'
+      );
+    }
+    if (!process.env.GITHUB_TOKEN || process.env.GITHUB_TOKEN.length === 0) {
+      throw new Error(
+        'You have to set the GITHUB_TOKEN in your secrets configuration'
+      );
+    }
+
+    const context = JSON.parse(
+      process.env.GITHUB_CONTEXT || ''
+    ) as GitHubContext;
+
+    const [owner, repo] = context.repository.split('/');
+
+    core.debug('debug message');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
